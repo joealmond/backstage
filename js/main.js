bb40990a45f5c65b13ad27c0879f7f07df501518
joealmond <jozsef.mandula@gmail.com>
2021-03-07 09:47:03 +0100

    Hide navbar by scroll. Re organizing CSS started.

--- js/main.js
+++ js/main.js
@@ -10,3 +10,18 @@ function myFunction() {
 //     this.classList.toggle("collapsible--expanded");
 //   })
 // );
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".nav").style.top = "0";
+    document.querySelector(".nav-button").style.top = "-25px";
+    document.querySelector(".nav-button").style.transitionDelay = "-0.5s";
+  } else {
+    document.querySelector(".nav").style.top = "-60px";
+    document.querySelector(".nav-button").style.top = "0";
+    document.querySelector(".nav-button").style.transitionDelay = "0.5s";
+  }
+  prevScrollpos = currentScrollPos;
+};
